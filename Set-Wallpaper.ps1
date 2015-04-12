e6cd3161f6b67e3a951428f061aeda2f1d4a4552
Dalmiro <dalmiro.granias@gmail.com>
2015-04-12 01:46:18 -0300

    changed approach by adding class
    source:
    http://stackoverflow.com/questions/9440135/powershell-script-from-shortcut-to-change-desktop

--- Set-Wallpaper.ps1
+++ Set-Wallpaper.ps1
@@ -17,7 +17,52 @@ Process
         throw "File $file does not exist"
     
     }
+
+    Add-Type @"
+using System;
+using System.Runtime.InteropServices;
+using Microsoft.Win32;
+namespace Wallpaper
+{
+   public enum Style : int
+   {
+       Tile, Center, Stretch, NoChange
+   }
+   public class Setter {
+      public const int SetDesktopWallpaper = 20;
+      public const int UpdateIniFile = 0x01;
+      public const int SendWinIniChange = 0x02;
+      [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+      private static extern int SystemParametersInfo (int uAction, int uParam, string lpvParam, int fuWinIni);
+      public static void SetWallpaper ( string path, Wallpaper.Style style ) {
+         SystemParametersInfo( SetDesktopWallpaper, 0, path, UpdateIniFile | SendWinIniChange );
+         RegistryKey key = Registry.CurrentUser.OpenSubKey("Control Panel\\Desktop", true);
+         switch( style )
+         {
+            case Style.Stretch :
+               key.SetValue(@"WallpaperStyle", "2") ; 
+               key.SetValue(@"TileWallpaper", "0") ;
+               break;
+            case Style.Center :
+               key.SetValue(@"WallpaperStyle", "1") ; 
+               key.SetValue(@"TileWallpaper", "0") ; 
+               break;
+            case Style.Tile :
+               key.SetValue(@"WallpaperStyle", "1") ; 
+               key.SetValue(@"TileWallpaper", "1") ;
+               break;
+            case Style.NoChange :
+               break;
+         }
+         key.Close();
+      }
+   }
+}
+"@
+
+[Wallpaper.Setter]::SetWallpaper( $file, 2 )
     
+   <#
     Set-ItemProperty -path 'HKCU:\Control Panel\Desktop\' -name Wallpaper -value $file
     Set-ItemProperty -path 'HKCU:\Control Panel\Desktop\' -name WallpaperStyle -value 2
     
@@ -26,10 +71,10 @@ Process
         
         rundll32.exe user32.dll, UpdatePerUserSystemParameters  
 
-        Start-Sleep -Seconds 2
+        #Start-Sleep -Seconds 2
 
     }
-
+    #>
 }
 End
 {
